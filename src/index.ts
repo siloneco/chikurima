d951af299c9df31093e9318ca569b774d8c6d801
m2en <me@m2en.dev>
2022-09-25 17:04:56 +0900

    feat: rechange log-system

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { Client, GatewayIntentBits, WebhookClient } from 'discord.js';
+import {
+  sendCommandConsole,
+  sendDBConsole,
+  sendWebhookConsole
+} from './util/logger.js';
 import { MessageReportCommand } from './command/messageReportCommand.js';
 import { ReportInfo } from './db/reportDB.js';
 import { Sequelize } from 'sequelize';
@@ -6,6 +11,7 @@ import { UserReportCommand } from './command/userReportCommand.js';
 import { deployCommand } from './command/deploy.js';
 import dotenv from 'dotenv';
 import { extractEnv } from './util/envManager.js';
+import { sendReport } from './util/sendReport.js';
 
 const client = new Client({
   intents: [
@@ -49,32 +55,55 @@ const sequelize = new Sequelize(name, username, password, {
 const webhookClient = new WebhookClient({ url: webhookUrl });
 
 client.on('ready', async () => {
+  const startTime = performance.now();
+
   if (client.user == null) {
-    throw new Error('Failed to identify client user');
+    throw new Error(
+      'クライアントユーザーの取得に失敗しました。起動できません。'
+    );
   }
+  console.log(`${client.user.tag}(${client.user.id}) に接続を開始しました。`);
 
   // mariadbとの接続
   try {
-    console.log('Connecting to database ....');
+    sendDBConsole('データベースに接続しています... (Step 1/3)');
     await sequelize.authenticate();
     ReportInfo(sequelize);
-    console.log('Successfully connected to database');
+    sendDBConsole('データベースへの接続に成功しました。');
   } catch (e) {
     console.log(e);
-    throw new Error('Failed to connect to database');
+    throw new Error('データベースへの接続に失敗しました。起動できません。');
   }
 
   // コマンドの登録
   try {
-    console.log('Registering commands ....');
+    sendCommandConsole('ギルドコマンドを登録中... (Step 2/3)');
     await deployCommand(client.user.id);
-    console.log('Command successfully registered');
+    sendCommandConsole('ギルドコマンドの登録に成功しました。(Step 2/3)');
   } catch (e) {
-    console.error(`Failed to register command`);
     console.error(e);
+    throw new Error('コマンドの登録に失敗しました。起動できません。');
   }
 
-  console.log(`${client.user.tag}(${client.user.id}) Ready!`);
+  try {
+    sendWebhookConsole(
+      'Webhookのテストを行っています... Webhookに開発者のIDが表示されます。確認してください。 (Step 3/3)'
+    );
+    await sendReport(
+      webhookClient,
+      '<@586824421470109716>',
+      '<@586824421470109716>',
+      'Webhookのテストです。',
+      undefined
+    );
+    sendWebhookConsole('Webhookのテストに成功しました。');
+  } catch (e) {
+    console.error(e);
+    throw new Error('Webhookのテストに失敗しました。起動できません。');
+  }
+  const endTime = performance.now();
+
+  console.log(`Done! (${endTime - startTime}ms)`);
 });
 
 client.on('interactionCreate', async (interaction) => {
@@ -86,7 +115,7 @@ client.on('interactionCreate', async (interaction) => {
   if (!guild) return;
   if (!interaction.guild.available) {
     await interaction.reply({
-      content: 'このギルドは利用できない状態です',
+      content: 'このギルドは利用できない状態です。運営に連絡してください。',
       ephemeral: true
     });
     return;
